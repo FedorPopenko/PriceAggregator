1dd77674ec8dfc5b7e8e7cca4b2633ecb78dcda9
radprofy <radprofy@gmail.com>
2024-06-27 18:10:48 +0400

    feat: adding database context

---
+++ PriceAggregator/DAL/GamesDB.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PriceAggregator.DAL
+{
+    public class GamesDB : DbContext
+    {
+        public DbSet<GameDTO> Games { get; set; }
+        public DbSet<PriceDTO> Prices { get; set; }
+
+        public GamesDB(DbContextOptions<GamesDB> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PriceDTO>()
+                .HasOne<GameDTO>()
+                .WithMany()
+                .HasForeignKey(f => f.GameId);
+        }
+
+    }
+
+    public class PriceDTO
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public string Currency { get; set; }
+        public string Country { get; set; }
+        public int GameId { get; set; }
+    }
+
+    public class GameDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string TitleIndex { get; set; }
+    }
+}
