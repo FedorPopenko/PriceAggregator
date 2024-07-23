b30433661c4493bb07bfcd02c06f4e87d9fb1659
radprofy <radprofy@gmail.com>
2024-07-23 20:33:55 +0400

    fix: cors

--- PriceAggregator/Program.cs
+++ PriceAggregator/Program.cs
@@ -3,30 +3,23 @@ using PriceAggregator.DAL;
 using PriceAggregator.Integrations;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContextFactory<GamesDB>(o => o.UseInMemoryDatabase("MyDatabase"));
-builder.Services.AddSingleton<IPriceSearcher,PriceSearcher>();
+builder.Services.AddSingleton<IPriceSearcher, PriceSearcher>();
 builder.Services.AddSingleton<ICurrencyRatesAPI, CurrencyRatesAPI>();
+builder.Services.AddCors(b =>
+    b.AddDefaultPolicy(p =>
+        p.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader()));
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
+app.UseCors();
 app.Run();
